dffc51dcb26464c3098e2dbca7decf9a5a0b140d
landroni <landroni@users.noreply.github.com>
2026-03-17 17:25:19 +0000

    set up import and make_dataset scripts

---
+++ notebooks/commands-exam-DVC.sh
@@ -0,0 +1,17 @@
+##activate venv
+cd accidents/examen-dvc/
+source ../Template_MLOps_accidents/env/bin/activate
+
+
+
+##create modified dataset
+python ./src/data/make_dataset.py
+# python ./src/data/make_dataset.py ./data/raw ./data/processed  ##doesn't work
+
+
+
+
+
+
+
+
